9b80a41599fce7ca07182139a0ba1ff7467dcc99
Leo Sheu <leo850611@users.noreply.github.com>
2017-04-15 02:34:08 +0800

    register system

--- fcuapp.py
+++ fcuapp.py
@@ -3,20 +3,14 @@ from flask import Flask,url_for,request,render_template,session,redirect,escape,
 import sqlite3
 import requests
 import json
-import os
+import re
 
 ## http://逢甲資訊.台灣/ Copyright (C) 2017 Leo Sheu.
 
-UPLOAD_FOLDER = 'classtable'
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
+app.config['UPLOAD_FOLDER'] = 'download'
 secretkey = 'Google reCAPTCHA key'
 
-google = {
-    'secret' : secretkey,
-    'response' : ''
-}
 
 @app.route('/' , methods = ['GET', 'POST'])
 def index():
@@ -26,27 +20,30 @@ def index():
                 'Account' : '',
                 'Password' : ''
             }
-            nid = request.form['nid']
-            fcu['Account'] = nid
-            fcu['Password'] = request.form['pwd']
-            classtable = requests.post("https://service206-sds.fcu.edu.tw/mobileservice/CourseService.svc/Timetable", json = fcu)
-            if 'true' in classtable.text:
-                table = classtable.json()['TimetableTw']
-                creattable(nid, classtable.json())
-                filename = nid+'.html'
-                return send_from_directory(app.config['UPLOAD_FOLDER'],filename,as_attachment=True)
-            
-        return 'Logged in as %s ' % escape(session['username']) + '''
-            <form action="" method="post"> 
-                <p><label>逢甲課表產生器</label>
-                <p><label>學號：</label>
-                <input type=text name=nid> 
-                <p><label>密碼：</label>
-                <input type=password name=pwd> 
-                <p><input type=submit value= 下載> 
-                <label>本系統使用NID驗證登入</label>
-                <img src="http://myweb.fcu.edu.tw/~d0381718/img/nid.gif" style="border-width:0px;" />
-        '''
+            nid = request.form['nid'].upper()
+            password = request.form['pwd']
+            if (len(nid)>5) and (len(password)>5):
+                header = {
+                    'Connection' : 'keep-alive',
+                    'Content-Type' : 'text/json',
+                    'Host' : 'service206-sds.fcu.edu.tw',
+                    'User-Agent' : 'Mozilla/5.0 (Linux; Android 5.1.1; D6653 Build/23.4.A.1.232; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/57.0.2987.132 Mobile Safari/537.36'
+                }
+                fcu['Account'] = nid
+                fcu['Password'] = password
+                classtable = requests.post("https://service206-sds.fcu.edu.tw/mobileservice/CourseService.svc/Timetable", json = fcu, headers = header)
+                if 'true' in classtable.text:
+                    table = classtable.json()['TimetableTw']
+                    language = 0
+                    if request.form['language'] != '1':
+                        language = 1
+                    creattable(nid, classtable.json(), language)
+                    filename = nid+'.html'
+                    return send_from_directory(app.config['UPLOAD_FOLDER'],filename,as_attachment=True)
+                else:
+                    return alertmsg('不要亂填害林杯被查水表好嗎(┛`д´)┛')
+
+        return 'Username : %s ' % escape(session['username']) + render_template('classtable.html')
     else:
         return redirect(url_for('login'))
     
@@ -57,39 +54,94 @@ def about():
 @app.route ( '/login', methods = ['GET', 'POST']) 
 def  login (): 
     if  request.method == 'POST' : 
-        google['response'] = request.form['g-recaptcha-response']
-        captcha = requests.post("https://www.google.com/recaptcha/api/siteverify", data = google)
-        if (True): #'true' in captcha.text
-            session['username']  =  request.form['username'] 
-            request.form['password']
-        return redirect(url_for('index')) 
-    return  ''' 
-        <form action="" method="post"> 
-            <p><label>帳號：</label>
-            <input type=text name=username> 
-            <p><label>密碼：</label>
-            <input type=password name=password> 
-            <script src='https://www.google.com/recaptcha/api.js' async defer></script>
-            <div class=g-recaptcha data-sitekey='6LdGuRwUAAAAAGZmBJNNiYYaofqOO9I45e-LP8c_' data-theme=dark ></div>
-            <p><input type=submit value= Login> 
-    '''
+        username = request.form['username'] 
+        password = request.form['password']
+        if (20>=len(username)>=5) and (20>=len(password)>=5):
+            conn = sqlite3.connect('iecs.db')
+            curs = conn.cursor()
+
+            curs.execute("SELECT pwd FROM user WHERE id=:id ", {"id": username})
+            
+            pwddata = curs.fetchall()
+            try:
+                pwd = str(pwddata[0])[2:-3]
+            except:
+                pwd = ''
+            if pwd == password:
+                session['username'] = username
+                return redirect(url_for('index'))
+    return render_template ('login.html')
 
     
 @app.route ( '/logout' ) 
-def  logout (): 
+def logout (): 
     session.pop('username', None) 
-    return redirect(url_for('index'))
+    return redirect(url_for('login'))
+
+@app.route ( '/register', methods = ['GET', 'POST'])    
+def register():
+    google = {
+        'secret' : secretkey,
+        'response' : ''
+    }
+    if  request.method == 'POST' : 
+        google['response'] = request.form['g-recaptcha-response']
+        captcha = requests.post("https://www.google.com/recaptcha/api/siteverify", data = google)
+        if ('true' in captcha.text): #True
+            id = request.form['username']
+            mail = request.form['email']
+            pwd1 = request.form['password1']
+            pwd2 = request.form['password2']
+            
+            if (pwd1 != pwd2):
+                return alertmsg('Error：兩次輸入密碼不一致')
+            if (len(id)<5) or (len(id)>20) or (len(pwd1)<5) or (len(pwd1)>20):
+                return alertmsg('Error：使用者名稱或密碼長度錯誤')
+            if (len(mail)<6) or (len(mail)>40):
+                return alertmsg('Error：email長度錯誤')
+            if re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", mail) == None:
+                return alertmsg('Error：email格式錯誤')
+            
+            conn = sqlite3.connect('iecs.db')
+            curs = conn.cursor()
+            try:
+                curs.execute('INSERT INTO user(id,mail,pwd) VALUES(?,?,?)', (id,mail,pwd1) )
+            except:
+                return alertmsg('ERROR：使用者名稱已存在')
+            conn.commit()
+            curs.execute('SELECT * from user')
+            print(curs.fetchall())
+            return alertmsg('註冊成功，請回登入頁面開始使用！')
+        else:
+            return alertmsg('你是機器人？')
+    return  render_template ( 'register.html')
+
+
+@app.route ( '/forget', methods = ['GET', 'POST'])    
+def forget():
+    google = {
+        'secret' : secretkey,
+        'response' : ''
+    }
+    return ""    
     
+    
+def alertmsg(msg):
+    return '''
+    <script>
+        alert(' ''' + msg + ''' ');
+        window.history.go(-1);
+    </script>'''
+
 def db_search (id, password):
     pass
     
     
-def db_add (id, password):
-    pass
-
-    
-def creattable (name, json):
-    timetable = json['TimetableTw']
+def creattable (name, json, language):
+    if language is 0:
+        timetable = json['TimetableTw']
+    else:
+        timetable = json['TimetableEn']
     row = ['                <tr class="row1">\n', '                <tr class="row2">\n']
     number = [
         '                    <td>第1節<br>08:10~09:00</td>\n',
@@ -107,7 +159,7 @@ def creattable (name, json):
         '                    <td>第13節<br>20:10~21:00</td>\n',
         '                    <td>第14節<br>21:10~22:00</td>\n'
     ]
-    f = open('classtable/'+name+'.html', 'w', encoding = 'utf-8')
+    f = open('download\\'+name+'.html', 'w', encoding = 'utf-8')
     f.write('''<!DOCTYPE>
     <html>
     <head>
@@ -124,7 +176,7 @@ def creattable (name, json):
         </style>
     </head>
     <body>
-        <h1>105學年度第2學期課程資訊(課表)</h1>
+        <h1>105學年度第2學期課程資訊</h1>
         <table>
             <thead>
                 <tr class="row0">
@@ -147,7 +199,10 @@ def creattable (name, json):
             for t in timetable:
                 if (t['SctWeek'] == day) and (t['SctPeriod'] == num):
                     f.write('                    <td>' + t['SubName'] + '<br>')
-                    f.write(t['RomName'])
+                    try:
+                        f.write(t['RomName'])
+                    except:
+                        pass
                     f.write('</td>\n')
                     flag = 1
             if (flag != 1):
@@ -161,12 +216,8 @@ def creattable (name, json):
     f.close()
 
 
-    
 if __name__ == '__main__':
-    conn = sqlite3.connect('user.db')
-    curs = conn.cursor()
-    #curs.execute('CREATE TABLE user(id VARCHAR(20) PRIMARY KEY, pass VARCHAR(20))')
-    
+    #curs.execute('CREATE TABLE user(id VARCHAR(20) PRIMARY KEY, mail VARCHAR(40), pwd VARCHAR(20))')
     app.secret_key = '4g^gE)5G-/g{qagby@Ug+sC<'
     app.debug = True 
     app.run(host = '0.0.0.0')
